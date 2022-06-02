272efd8e2c9c75c74a1b2cc1a7963d8fe654fd5d
Kacper Nowak <kacpernowak@pop-os.localdomain>
2022-06-03 00:08:19 +0200

    Initial commit
    Signed-off-by: Kacper Nowak <kacpernowak@pop-os.localdomain>

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <SDL2/SDL.h>
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <vector>
+
+#define errcheck(e)                                                            \
+  {                                                                            \
+    if (e) {                                                                   \
+      std::cout << SDL_GetError() << std::endl;                                \
+      SDL_Quit();                                                              \
+      return -1;                                                               \
+    }                                                                          \
+  }
+
+int main(int , char **) {
+  const int width = 640;
+  const int height = 480;
+
+  errcheck(SDL_Init(SDL_INIT_VIDEO) != 0);
+
+  SDL_Window *window = SDL_CreateWindow(
+      "initial_demo", SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+  errcheck(window == nullptr);
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED);
+  errcheck(renderer == nullptr);
+
+  std::chrono::milliseconds dt(15);
+
+  std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
+  for (bool game_active = true; game_active;) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT)
+        game_active = false;
+    }
+
+    SDL_RenderPresent(renderer);
+
+    std::this_thread::sleep_until(current_time = current_time + dt);
+  }
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
