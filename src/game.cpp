ad54185d4899b304cb203bcb4a3adee126029c7c
Sequindo <chacper@pm.me>
2024-05-19 02:25:05 +0200

    Initial spritesheet handling; experimental
    Signed-off-by: Sequindo <chacper@pm.me>

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,7 @@
 #include "game.hpp"
 
 #include "SDL.h"
+#include "SDL_image.h"
 #include <iostream>
 
 Game::Game() {
@@ -24,6 +25,15 @@ void Game::init(bool fullscreen) {
             SDL_WINDOW_SHOWN);
         
         renderer = SDL_CreateRenderer(window, -1, 0);
+        {
+            SDL_Surface *tmpSurface = IMG_Load("../resources/spritesheets/at-rest-facing-right-x720-y512-per-frame.png");
+            cowRestTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+            SDL_FreeSurface(tmpSurface);
+
+            tmpSurface = IMG_Load("../resources/cowspritesheetx=720pxY=512px.png");
+            cowMovingTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+            SDL_FreeSurface(tmpSurface);
+        }
     } else {
         std::cerr << SDL_GetError() << std::endl;
         isGameRunning = false;
@@ -49,6 +59,8 @@ void Game::update() {
 void Game::render() {
     SDL_RenderClear(renderer);
     //here we add stuff for rendering
+    SDL_RenderCopy(renderer, cowRestTexture, nullptr, nullptr);
+    //end
     SDL_RenderPresent(renderer);
 }
 
--- src/game.hpp
+++ src/game.hpp
@@ -4,9 +4,10 @@
 //forward declaration of SDL - related stuff
 struct SDL_Renderer;
 typedef struct SDL_Renderer SDL_Renderer;
-
 struct SDL_Window;
 typedef struct SDL_Window SDL_Window;
+struct SDL_Texture;
+typedef struct SDL_Texture SDL_Texture;
 
 namespace GameConstants {
     inline const char* title {"Untitled Cow Gore Game"};
@@ -30,6 +31,9 @@ private:
     //SDL stuff
     SDL_Window *window = nullptr;
     SDL_Renderer *renderer = nullptr;
+
+    SDL_Texture* cowRestTexture = nullptr;
+    SDL_Texture* cowMovingTexture = nullptr;
 };
 
 #endif
